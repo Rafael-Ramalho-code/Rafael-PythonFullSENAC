e0fb5e2cfe96e69e63dcc7f51a8281e31c30ed2d
Rafael-Ramalho-code <rafaelramalho1807@gmail.com>
2025-08-13 18:49:03 -0300

    exercício javascript

---
+++ aula04_teste.js
@@ -0,0 +1,15 @@
+
+
+
+let numeroA = 10;
+let numeroB = 20;
+let numeroC = 30;
+
+let soma = numeroA + numeroB + numeroC;
+console.log("A soma dos numeros é: " + soma);
+
+let multiplicacao = numeroA * numeroB;
+console.log("A multiplicação dos números é " + multiplicacao);
+
+let divisao = numeroC / numeroB;
+console.log("A divisão dos números é " + divisao)
